100a0d17bab76736b5127dc623dfb01382e5997e
Raul <rauldiazlarraga@gmail.com>
2024-04-28 17:03:00 +0200

    ssg redirects

--- server-ssg.js
+++ server-ssg.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import fse from 'fs-extra'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
 const app = express()
+const dir = dirname(fileURLToPath(import.meta.url))
 app.use(express.static('dist/client'));
+app.use((req, res) => {
+    const i = path.join(dir, 'dist/client/' + req.path + '.html')
+    if(fse.pathExistsSync(i)) {
+        console.log({i})
+        res.sendFile(i)
+    } else {
+        res.redirect('/')
+    }
+});
 app.listen(8080, ()=>{
     console.log('server listen on port: https://localhost:8080 ', )
 })
